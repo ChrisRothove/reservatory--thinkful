3eb64fff95ffe3132aca8ccd90884bc3d9c3925c
featherwalk <vacantorange@gmail.com>
2021-06-26 15:38:43 -0500

    small changes to dependencies and reservations controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,11 @@
+const service = require("./reservations.service");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
   res.json({
-    data: [],
+    data: await service.list(),
   });
 }
 
