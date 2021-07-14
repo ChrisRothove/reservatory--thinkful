f18696bbc7d7a9c9b229ed47d6e7447ba9f59d7e
ChrisRothove <vacantorange@gmail.com>
2021-07-13 19:39:57 -0500

    updated NewTable to include button click handlers

--- front-end/src/components/NewTable.js
+++ front-end/src/components/NewTable.js
@@ -1,6 +1,27 @@
-import { formatAsDate } from "../../../back-end/src/utils/date-time";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 export default function NewTable() {
+  const history = useHistory();
+  const defaultFormData = {
+    table_name: "",
+    table_capacity: 1,
+  };
+
+  const [formData, setFormData] = useState({ ...defaultFormData });
+
+  function handleChange(e) {
+    e.preventDefault();
+    const event = e.target;
+    setFormData({ ...formData, [event.name]: event.value });
+  }
+
+  function handleCancel(e) {
+    e.preventDefault();
+    setFormData({ ...defaultFormData });
+    history.goBack();
+  }
+
   return (
     <form className="resList form">
       <label>
@@ -9,25 +30,32 @@ export default function NewTable() {
           type="text"
           id="tableName"
           name="table_name"
+          value={formData.table_name}
           minlength="2"
           required
+          onChange={handleChange}
         ></input>
+        <div className="insight">min: 2 characters</div>
       </label>
-      <div className="insight">min: 2 characters</div>
       <label>
         Capacity:
         <input
           type="number"
           id="tableCapacity"
           name="table_capacity"
+          value={formData.table_capacity}
           min="1"
+          required
+          onChange={handleChange}
         ></input>
       </label>
       <div className="buttons">
         <button className="btn btn-success" type="submit">
           Submit
         </button>
-        <button className="btn btn-danger">Cancel</button>
+        <button className="btn btn-danger" onClick={handleCancel}>
+          Cancel
+        </button>
       </div>
     </form>
   );
